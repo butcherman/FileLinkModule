e7e69c85e7c6143c461b1aa94d3b32ed4f4a0f27
Butcherman <ronbutcher@att.net>
2022-05-04 19:50:13 -0700

    Rebuilt Routes

--- Config/config.php
+++ Config/config.php
@@ -5,7 +5,7 @@ return [
     *   Basic Module Information - Name and Version
     */
     'name' => 'FileLinkModule',
-    'ver'  => '1.0.2',
+    'ver'  => '1.0.3',
 
     /*
     *   Information for the Navbar
--- Routes/web.php
+++ Routes/web.php
@@ -1,55 +1,53 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 use Modules\FileLinkModule\Http\Controllers\AdminController;
 use Modules\FileLinkModule\Http\Controllers\FileLinkFileController;
 use Modules\FileLinkModule\Http\Controllers\FileLinkModuleController;
 use Modules\FileLinkModule\Http\Controllers\GuestFileUploadController;
 
-Route::prefix('file-links')->name('FileLinkModule.')->group(function()
-{
-    Route::get('{hash}',      [GuestFileUploadController::class, 'show'])->name('guest');
-    Route::get('show/{hash}', [GuestFileUploadController::class, 'show']);
-    Route::put('{hash}',      [GuestFileUploadController::class, 'update'])->name('guest.update');
-});
-
-Route::prefix('links')->name('FileLinkModule.')->group(function()
+Route::name('FileLinkModule.')->group(function()
 {
     /**
-     * Authorized User routes for using File Links
+     * Guest routes
+     * Note - authorized users can visit the routes as well without being redirected
      */
-    Route::middleware('auth')->group(function() {
-        Route::put(   'files/{id}/update',     [FileLinkFileController::class, 'update']) ->name('files.update');
-        Route::delete('files/{id}/destroy',    [FileLinkFileController::class, 'destroy'])->name('files.destroy');
+    Route::prefix('file-links')->group(function()
+    {
+        Route::get('show/{hash}', [GuestFileUploadController::class, 'show']);
+        Route::get('{hash}',      [GuestFileUploadController::class, 'show'])  ->name('guest');
+        Route::put('{hash}',      [GuestFileUploadController::class, 'update'])->name('guest.update');
+    });
 
-        Route::get(    '{id}/edit',   [FileLinkModuleController::class, 'edit'])   ->name('edit')  ->breadcrumb('Edit File Link', '.show');
-        Route::delete( '{id}/delete', [FileLinkModuleController::class, 'destroy'])->name('destroy');
-        Route::get(    'create',      [FileLinkModuleController::class, 'create']) ->name('create')->breadcrumb('New File Link', '.index');
-        Route::put(    '{id}',        [FileLinkModuleController::class, 'update']) ->name('update');
-        Route::get(    '{id}',        [FileLinkModuleController::class, 'show'])   ->name('show')  ->breadcrumb('Link Details', '.index');
-        Route::post(   '/',           [FileLinkModuleController::class, 'store'])  ->name('store');
-        Route::get(    '/',           [FileLinkModuleController::class, 'index'])  ->name('index') ->breadcrumb('File Links');
+    /**
+     * Authorized User routes
+     */
+    Route::middleware('auth')->group(function()
+    {
+        /**
+         * Administration routes
+         */
+        Route::prefix('links/administration')->name('admin.')->group(function()
+        {
+            Route::get('/',                 [AdminController::class, 'index'])->name('index')->breadcrumb('File Links', 'admin.index');
+            Route::get('{username}',        [AdminController::class, 'show'])->name('show')  ->breadcrumb('User Links', '.index');
+        });
 
         /**
-         * Administration routes for Module
+         * Standard File Link Routes
          */
-        Route::prefix('administration/file-link-module')->name('admin.')->group(function()
+        Route::prefix('links')->group(function()
         {
-            Route::get('/',           [AdminController::class, 'index'])->name('index');
-            Route::get('{username}',  [AdminController::class, 'show'])->name('show');
+            Route::put(   'files/{id}/update',     [FileLinkFileController::class, 'update']) ->name('files.update');
+            Route::delete('files/{id}/destroy',    [FileLinkFileController::class, 'destroy'])->name('files.destroy');
+
+            Route::get(    '{id}/edit',            [FileLinkModuleController::class, 'edit'])   ->name('edit')  ->breadcrumb('Edit File Link', '.show');
+            Route::delete( '{id}/delete',          [FileLinkModuleController::class, 'destroy'])->name('destroy');
+            Route::get(    'create',               [FileLinkModuleController::class, 'create']) ->name('create')->breadcrumb('New File Link', '.index');
+            Route::put(    '{id}',                 [FileLinkModuleController::class, 'update']) ->name('update');
+            Route::get(    '{id}',                 [FileLinkModuleController::class, 'show'])   ->name('show')  ->breadcrumb('Link Details', '.index');
+            Route::post(   '/',                    [FileLinkModuleController::class, 'store'])  ->name('store');
+            Route::get(    '/',                    [FileLinkModuleController::class, 'index'])  ->name('index') ->breadcrumb('File Links');
         });
     });
+
 });
-
-
-
