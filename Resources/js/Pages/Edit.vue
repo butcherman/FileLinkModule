cbe9261e74d0696ecdce6c48b9d8fe0fa0e97877
Butcherman <ronbutcher@att.net>
2022-06-21 19:54:55 -0700

    Added Copy Link Ability

--- Resources/js/Pages/Edit.vue
+++ Resources/js/Pages/Edit.vue
@@ -14,19 +14,53 @@
                                 <form-loader></form-loader>
                             </template>
                             <ValidationObserver v-slot="{handleSubmit}">
-                                <b-form @submit.prevent="handleSubmit(submitForm)" novalidate>
-                                    <text-input v-model="form.link_name" label="Link Name" name="name" placeholder="Enter a user friendly name for this link" rules="required"></text-input>
-                                    <date-picker v-model="form.expire" label="Expires Date" name="expires" placeholder="Select the date the link expires" rules="required"></date-picker>
-                                    <b-form-checkbox v-model="form.allow_upload" switch>
+                                <b-form
+                                    @submit.prevent="handleSubmit(submitForm)"
+                                    novalidate
+                                >
+                                    <text-input
+                                        v-model="form.link_name"
+                                        name="name"
+                                        rules="required"
+                                        label="Link Name"
+                                        placeholder="Enter a user friendly name for this link"
+                                    />
+                                    <date-picker
+                                        v-model="form.expire"
+                                        name="expires"
+                                        rules="required"
+                                        label="Expires Date"
+                                        placeholder="Select the date the link expires"
+                                    />
+                                    <b-form-checkbox
+                                        v-model="form.allow_upload"
+                                        switch
+                                    >
                                         Allow Visitor to Upload Files
                                     </b-form-checkbox>
                                     <div class="my-2 text-center">
-                                        <b-button switch variant="info" @click="form.has_instructions = !form.has_instructions">{{instructionText}}</b-button>
+                                        <b-button
+                                            switch
+                                            variant="info"
+                                            @click="form.has_instructions = !form.has_instructions"
+                                        >
+                                            {{instructionText}}
+                                        </b-button>
                                     </div>
-                                    <b-collapse id="instructions" :visible="form.has_instructions">
-                                        <text-editor v-model="form.instructions" placeholder="Enter Instructions" label="Instructions"></text-editor>
+                                    <b-collapse
+                                        id="instructions"
+                                        :visible="form.has_instructions"
+                                    >
+                                        <text-editor
+                                            v-model="form.instructions"
+                                            label="Instructions"
+                                            placeholder="Enter Instructions"
+                                        />
                                     </b-collapse>
-                                    <submit-button button_text="Update File Link" :submitted="submitted"></submit-button>
+                                    <submit-button
+                                        button_text="Update File Link"
+                                        :submitted="submitted"
+                                    />
                                 </b-form>
                             </ValidationObserver>
                         </b-overlay>
@@ -60,24 +94,13 @@
                 }),
             }
         },
-        created() {
-            //
-        },
-        mounted() {
-            //
-        },
         computed: {
-            //
             instructionText()
             {
                 return this.form.has_instructions ? 'Remove Instructions' : 'Add Instructions';
             }
         },
-        watch: {
-            //
-        },
         methods: {
-            //
             submitForm()
             {
                 this.submitted = true;
@@ -85,10 +108,6 @@
                 {
                     this.form.instructions = null;
                 }
-
-                // this.$inertia.put(route('FileLinkModule.update', this.details.link_id), this.form);
-
-                console.log(this.form);
 
                 this.form.put(route('FileLinkModule.update', this.details.link_id));
             },
